cc0386ac7d52b506c60d916d11c5a96a54dea256
Eric Nichols <ebn646@hotmail.com>
2024-04-24 15:33:15 -0400

    removed auth actions

--- actions/auth/auth-actions.ts
+++
@@ -1,9 +0,0 @@
-//export function to sign in users with email and password
-export const signin = async (email: string, password: string) => {
-  try {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    return userCredential;
-  } catch (error) {
-    return error;
-  }
-};
